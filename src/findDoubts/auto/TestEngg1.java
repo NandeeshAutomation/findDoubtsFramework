f4acd6bc6b410e72e4db0518b1e22a3cfbab121a
NandeeshAutomation <nandeeshautomation@gmail.com>
2025-07-04 13:25:00 +0530

    Third Commit in TestEngg1

--- src/findDoubts/auto/TestEngg1.java
+++ src/findDoubts/auto/TestEngg1.java
@@ -1,5 +1,37 @@
 package findDoubts.auto;
 
-public class TestEngg1 {
+import java.util.ArrayList;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestEngg1 
+{
+	@Test
+	public void myTest() throws InterruptedException
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.fireflink.com/");
+		Thread.sleep(5000);
+		//driver.findElement(By.xpath("//a[text()='Sign In']")).click();
+		Thread.sleep(1000);
+		JavascriptExecutor js =(JavascriptExecutor)(driver);
+		js.executeScript("window.open()");
+		Thread.sleep(1000);
+		ArrayList list = new ArrayList<>(driver.getWindowHandles());
+		//driver.switchTo().newWindow(WindowType.TAB);
+		driver.switchTo().window((String) list.get(1));
+		Thread.sleep(1000);
+		driver.get("https://www.google.com/");
+		Thread.sleep(1000);
+		
+		
+		
+		
+	}
 }
